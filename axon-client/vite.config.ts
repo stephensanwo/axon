2095aea0b419f1ac363b01a7bb3024cd4374206e
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-01-02 18:35:40 +0000

    axon client updates -wip

--- axon-client/vite.config.ts
+++ axon-client/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -12,5 +15,10 @@ export default defineConfig({
   server: {
     port: 4514,
     host: "0.0.0.0",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
   },
 });
