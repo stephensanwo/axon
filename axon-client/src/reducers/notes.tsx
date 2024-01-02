ee00e267514562eb294c7287f514d59346fb9bab
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-01-02 18:30:13 +0000

    update global reducers

--- axon-client/src/reducers/notes.tsx
+++ axon-client/src/reducers/notes.tsx
@@ -1,13 +1,4 @@
-import { ColorPalette } from "src/shared/themes";
-import { nodeEvents } from "src/types/node";
-import {
-  INode,
-  INote,
-  INoteAction,
-  NodeDataProps,
-  NodeStyleProps,
-} from "src/types/notes";
-import { v4 as uuidv4 } from "uuid";
+import { INote, INoteAction } from "src/types/notes";
 
 const noteReducer = (note: INote, action: INoteAction) => {
   switch (action.type) {
@@ -15,64 +6,17 @@ const noteReducer = (note: INote, action: INoteAction) => {
       return action.payload;
     }
 
-    case "ADD_NODE": {
-      let id = uuidv4();
-
-      const lastNode =
-        note.nodes.length > 0 ? note.nodes[note.nodes.length - 1] : null;
-      const newX = lastNode
-        ? lastNode.position.x + 100 + 280
-        : window.innerWidth / 2.5;
-      const newY = lastNode
-        ? lastNode.position.y + 0
-        : window.outerHeight / 2.5;
-      const newNode = {
-        id: id,
-        type: "input",
-        data: {
-          id: id,
-          title: "Text Box",
-          description: "Text Description",
-          category: action.payload.node_type,
-          node_styles: {
-            node_background_color:
-              action.payload.default_theme.node_background_color,
-            node_border_color: action.payload.default_theme.node_border_color,
-            font_color: action.payload.default_theme.font_color,
-          } as NodeStyleProps,
-        } as NodeDataProps,
-        position: {
-          x: newX,
-          y: newY,
-        },
-        className: action.payload.node_type,
-        content: "",
-      } as INode;
-
-      // Send Node Event
-      action.payload.eventFn &&
-        action.payload.eventFn(nodeEvents.ADD_NODE, newNode);
-
-      return { ...note, nodes: [...note.nodes, newNode] };
+    case "UPDATE_NODES": {
+      return {
+        ...note,
+        nodes: action.payload,
+      };
     }
-
-    case "UPDATE_NODE": {
-      const { node_id, updated_fields } = action.payload;
-
-      const updatedNodes = note.nodes?.map((node) => {
-        if (node.id === node_id) {
-          return {
-            ...node,
-            data: {
-              ...node.data,
-              ...updated_fields,
-            },
-          };
-        }
-        return node;
-      });
-
-      return { ...note, nodes: updatedNodes };
+    case "UPDATE_EDGES": {
+      return {
+        ...note,
+        edges: action.payload,
+      };
     }
 
     default: {
