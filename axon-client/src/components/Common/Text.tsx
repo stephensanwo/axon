9548d91b873542bcc5ef7d8a4005d86358bc7d7f
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-07-08 23:33:54 +0100

    checkpoint: text

--- axon-client/src/components/Common/Text.tsx
+++ axon-client/src/components/Common/Text.tsx
@@ -14,7 +14,12 @@ function SmallTextSecondary(props: TextProps) {
   );
 }
 
+function Heading4(props: TextProps) {
+  return <PrimerText {...props} sx={{ fontSize: 1, fontWeight: 600 }} />;
+}
+
 export const Text = {
   SmallText,
   SmallTextSecondary,
+  Heading4,
 };
