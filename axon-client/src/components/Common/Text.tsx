746121148e1d18a6a02f4cb290d49d437b2915c9
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-07-12 23:52:04 +0100

    checkpoint: text

--- axon-client/src/components/Common/Text.tsx
+++ axon-client/src/components/Common/Text.tsx
@@ -1,10 +1,10 @@
 import { Text as PrimerText, TextProps, useTheme } from "@primer/react";
 
-function SmallText(props: TextProps) {
+function Small(props: TextProps) {
   return <PrimerText {...props} sx={{ fontSize: 0 }} />;
 }
 
-function SmallTextSecondary(props: TextProps) {
+function SmallSecondary(props: TextProps) {
   const { theme } = useTheme();
   return (
     <PrimerText
@@ -14,12 +14,37 @@ function SmallTextSecondary(props: TextProps) {
   );
 }
 
+function Paragraph(props: TextProps) {
+  return <PrimerText {...props} sx={{ fontSize: 1 }} />;
+}
+
+function ParagraphSecondary(props: TextProps) {
+  const { theme } = useTheme();
+  return (
+    <PrimerText
+      {...props}
+      sx={{ fontSize: 1, color: theme?.colors.text.gray }}
+    />
+  );
+}
 function Heading4(props: TextProps) {
+  return <PrimerText {...props} sx={{ fontSize: 2, fontWeight: 600 }} />;
+}
+
+function Heading5(props: TextProps) {
   return <PrimerText {...props} sx={{ fontSize: 1, fontWeight: 600 }} />;
 }
 
+function Heading6(props: TextProps) {
+  return <PrimerText {...props} sx={{ fontSize: 0, fontWeight: 600 }} />;
+}
+
 export const Text = {
-  SmallText,
-  SmallTextSecondary,
+  Small,
+  SmallSecondary,
   Heading4,
+  Heading5,
+  Heading6,
+  Paragraph,
+  ParagraphSecondary,
 };
