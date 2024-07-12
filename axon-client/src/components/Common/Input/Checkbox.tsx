193f1f9fa2ccbc5533d7af3a06267603d880ed0b
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-07-12 23:51:22 +0100

    checkpoint: checkbox

---
+++ axon-client/src/components/Common/Input/Checkbox.tsx
@@ -0,0 +1,73 @@
+import { Box, FormControl, Checkbox as PrimerCheckbox } from "@primer/react";
+import { Updater } from "@tanstack/react-form";
+
+function Checkbox({
+  label,
+  value,
+  placeholder,
+  error,
+  caption,
+  onChange,
+  onBlur,
+  leadingVisual,
+  trailingVisual,
+  trailingAction,
+  required = false,
+  htmlFor,
+  type,
+  visuallyHiddenLabel = false,
+}: {
+  label: string;
+  error: string | null;
+  required?: boolean;
+  caption?: string;
+  placeholder?: string;
+  value: string;
+  onChange: (updater: Updater<string>) => void;
+  onBlur?: () => void;
+  leadingVisual?: React.ReactNode;
+  trailingVisual?: React.ReactNode;
+  trailingAction?:
+    | React.ReactElement<
+        React.HTMLProps<HTMLButtonElement>,
+        string | React.JSXElementConstructor<any>
+      >
+    | undefined;
+  htmlFor: string;
+  type: React.HTMLInputTypeAttribute | undefined;
+  visuallyHiddenLabel?: boolean;
+}) {
+  return (
+    <FormControl
+      sx={{
+        width: "100%",
+      }}
+      required={required}
+    >
+      <FormControl.Label
+        sx={{
+          fontWeight: 400,
+          fontSize: 0,
+        }}
+        htmlFor={htmlFor}
+        visuallyHidden={visuallyHiddenLabel}
+      >
+        {label}
+      </FormControl.Label>
+      <PrimerCheckbox />
+      {error && (
+        <FormControl.Validation
+          variant={"error"}
+          sx={{
+            fontWeight: 400,
+          }}
+        >
+          {error}
+        </FormControl.Validation>
+      )}
+      <FormControl.Caption>{caption}</FormControl.Caption>
+    </FormControl>
+  );
+}
+
+export default Checkbox;
