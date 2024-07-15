61a5ef653a90b96c5fb8720e61f628f8fb74d939
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-07-15 23:45:34 +0100

    checkpoint: useDataMutation

---
+++ axon-client/src/hooks/api/useDataMutation.tsx
@@ -0,0 +1,43 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+interface DataMutationProps<D, R> {
+  mutationFn: (dto: D) => Promise<R>;
+  optionalQueryKeysToInvalidate?: string[][];
+  onSuccessCallback?: () => void;
+}
+
+export function useDataMutation<D, R>({
+  mutationFn,
+  optionalQueryKeysToInvalidate,
+  onSuccessCallback,
+}: DataMutationProps<D, R>) {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<R, unknown, D>({
+    mutationFn: async (dto) => mutationFn(dto),
+    onMutate: async (data) => {
+      console.log("onMutate", data);
+      return data;
+    },
+    onError: (error, variables, context) => {
+      console.error("onError", error, variables, context);
+    },
+    onSettled: (data, error, variables, context) => {
+      console.log("onSettled", data, error, variables, context);
+    },
+    onSuccess: (data, variables, context) => {
+      if (onSuccessCallback) {
+        onSuccessCallback();
+      }
+      console.log("onSuccess", data, variables, context);
+      // Invalidate any additional queries
+      if (optionalQueryKeysToInvalidate) {
+        optionalQueryKeysToInvalidate.forEach((queryKey) => {
+          queryClient.invalidateQueries({ queryKey });
+        });
+      }
+    },
+  });
+
+  return mutation;
+}
