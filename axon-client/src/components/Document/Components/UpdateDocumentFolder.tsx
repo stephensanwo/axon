9963988fb3aa1662e55346c423f9e823cbbbcaec
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-07-20 23:19:15 +0100

    checkpoint: UpdateDocumentFolder

---
+++ axon-client/src/components/Document/Components/UpdateDocumentFolder.tsx
@@ -0,0 +1,151 @@
+import { Box, Button, IconButton } from "@primer/react";
+import { LuFolderEdit } from "react-icons/lu";
+import OverlayMenu from "../../Common/OverlayMenu";
+import { Text } from "../../Common/Text";
+import { formOptions, useForm } from "@tanstack/react-form";
+import { InlineSpinner } from "../../Common/Spinner";
+import { useDocument } from "src/context/document/hooks/useDocument";
+import {
+  DocumentAction,
+  DocumentState,
+} from "src/context/document/document.types";
+import { UpdateDocumentFolderDto } from "src/domain/document/document.dto";
+import { DocumentFolderData } from "src/domain/document/document.entity";
+import { Input } from "src/components/Common/Input";
+import { formValidation } from "src/common/forms/forms.validation";
+import { zodValidator } from "@tanstack/zod-form-adapter";
+import { PiFolderBold } from "react-icons/pi";
+
+function UpdateDocumentFolder({
+  documentState,
+}: {
+  documentState: DocumentState;
+  documentStateDispatch: React.Dispatch<DocumentAction>;
+}) {
+  const { updateDocumentFolder } = useDocument();
+  const {
+    documentFolders: { selectedDocumentFolders },
+  } = documentState;
+
+  // UpdateDocumentFolder.tsx is only rendered when a single folder is selected
+  const folderData = selectedDocumentFolders[0];
+
+  const formOpts = formOptions<DocumentFolderData>({
+    defaultValues: {
+      name: folderData?.name || "",
+      description: folderData?.description || "",
+    },
+  });
+
+  const Form = useForm({
+    ...formOpts,
+    onSubmit: async ({ value }) => {
+      const dto: UpdateDocumentFolderDto = {
+        ...folderData,
+        ...value,
+      };
+      updateDocumentFolder.mutate(dto);
+    },
+  });
+
+  return (
+    <OverlayMenu
+      width={300}
+      minHeight={150}
+      side="outside-top"
+      anchorOffset={10}
+      alignmentOffset={-150}
+      anchorComponent={
+        <IconButton
+          variant="default"
+          icon={LuFolderEdit}
+          disabled={false}
+          aria-label="Update Selected Folder"
+          sx={{
+            flexShrink: 0,
+          }}
+        />
+      }
+      heading={<Text.Heading5>Update Selected Folder</Text.Heading5>}
+    >
+      <Box
+        sx={{
+          p: 3,
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+        as="form"
+      >
+        <Form.Field
+          name="name"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChangeAsyncDebounceMs: 500,
+            onChange: formValidation.fieldValidation("string", 50),
+          }}
+        >
+          {({ state, handleChange, handleBlur }) => {
+            return (
+              <Input.Text
+                label="Name"
+                placeholder="e.g. My Folder"
+                value={state.value || ""}
+                error={formValidation.fieldError(state.meta)}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                leadingVisual={<PiFolderBold />}
+                required={true}
+                htmlFor="update-document-folder-name"
+                type="text"
+                caption="Max 50 characters"
+              />
+            );
+          }}
+        </Form.Field>
+        <Form.Field
+          name="description"
+          validatorAdapter={zodValidator()}
+          validators={{
+            onChangeAsyncDebounceMs: 500,
+            onChange: formValidation.fieldValidation("string", 250),
+          }}
+        >
+          {({ state, handleChange, handleBlur }) => {
+            return (
+              <Input.TextArea
+                label="Short Description"
+                placeholder="e.g. Project Folder"
+                rows={2}
+                resize="none"
+                value={state.value || ""}
+                error={formValidation.fieldError(state.meta)}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                caption="Max 250 characters"
+                required={true}
+                htmlFor="update-document-folder-description"
+              />
+            );
+          }}
+        </Form.Field>
+        <Form.Subscribe
+          selector={({ isSubmitting }) => [isSubmitting]}
+          children={([isSubmitting]) => (
+            <Button
+              variant="primary"
+              leadingVisual={isSubmitting ? InlineSpinner : LuFolderEdit}
+              disabled={isSubmitting}
+              onClick={Form.handleSubmit}
+              size="medium"
+            >
+              {isSubmitting ? "Updating Folder..." : "Update Folder"}
+            </Button>
+          )}
+        />
+      </Box>
+    </OverlayMenu>
+  );
+}
+
+export default UpdateDocumentFolder;
