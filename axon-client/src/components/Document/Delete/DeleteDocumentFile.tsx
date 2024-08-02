27b5999ea4736910fa27076446170ce9ea1faeb6
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-08-02 23:31:56 +0100

    checkpoint: delete document

---
+++ axon-client/src/components/Document/Delete/DeleteDocumentFile.tsx
@@ -0,0 +1,107 @@
+import { Box, Button, IconButton } from "@primer/react";
+import { PiTrashBold } from "react-icons/pi";
+import OverlayMenu from "../../Common/OverlayMenu";
+import { Text } from "../../Common/Text";
+import { useForm } from "@tanstack/react-form";
+import { InlineSpinner } from "../../Common/Spinner";
+import { useDocument } from "src/context/document/hooks/useDocument";
+import map from "lodash/map";
+import { useMemo } from "react";
+import { BaseDocumentProps } from "../index.types";
+
+function DeleteDocumentFile({ documentState }: BaseDocumentProps) {
+  const { deleteDocumentFile } = useDocument();
+  const {
+    documentFolderFiles: { selectedDocumentFiles },
+  } = documentState;
+  const isMultipleFilesSelected = selectedDocumentFiles.length > 1;
+
+  const selectedFileIds = useMemo(
+    () => map(selectedDocumentFiles, "id"),
+    [selectedDocumentFiles]
+  );
+
+  const Form = useForm({
+    onSubmit: async ({ value }) => {
+      deleteDocumentFile.mutate(selectedFileIds);
+    },
+  });
+
+  return (
+    <OverlayMenu
+      width={300}
+      minHeight={150}
+      side="outside-top"
+      anchorOffset={10}
+      alignmentOffset={0}
+      align="center"
+      anchorComponent={
+        isMultipleFilesSelected ? (
+          <Button
+            variant="danger"
+            leadingVisual={PiTrashBold}
+            trailingVisual={() => (
+              <Text.Heading6>{selectedDocumentFiles.length}</Text.Heading6>
+            )}
+            disabled={false}
+            aria-label="Delete Selected Files"
+            sx={{
+              flexShrink: 0,
+            }}
+          >
+            Delete
+          </Button>
+        ) : (
+          <IconButton
+            variant="danger"
+            icon={PiTrashBold}
+            disabled={false}
+            aria-label="Delete Selected File"
+            sx={{
+              flexShrink: 0,
+            }}
+          />
+        )
+      }
+      heading={<Text.Heading5>Delete Selected File(s)</Text.Heading5>}
+      onCloseCallback={() => {}}
+    >
+      <Box
+        sx={{
+          p: 3,
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+        as="form"
+      >
+        <Text.ParagraphSecondary
+          sx={{
+            textAlign: "center",
+            marginBottom: 1,
+          }}
+        >
+          {`Are you sure you want to delete ${isMultipleFilesSelected ? "these files" : "this file"}? This action cannot be undone.`}
+        </Text.ParagraphSecondary>
+        <Form.Subscribe
+          selector={({ isSubmitting }) => [isSubmitting]}
+          children={([isSubmitting]) => (
+            <Button
+              variant="danger"
+              leadingVisual={isSubmitting ? InlineSpinner : PiTrashBold}
+              disabled={isSubmitting}
+              onClick={Form.handleSubmit}
+              size="medium"
+            >
+              {isSubmitting
+                ? `Deleting ${isMultipleFilesSelected ? "Files" : "File"}...`
+                : `Delete ${isMultipleFilesSelected ? "Files" : "File"}`}
+            </Button>
+          )}
+        />
+      </Box>
+    </OverlayMenu>
+  );
+}
+
+export default DeleteDocumentFile;
---
+++ axon-client/src/components/Document/Delete/DeleteDocumentFolder.tsx
@@ -0,0 +1,107 @@
+import { Box, Button, IconButton } from "@primer/react";
+import { PiTrashBold } from "react-icons/pi";
+import OverlayMenu from "../../Common/OverlayMenu";
+import { Text } from "../../Common/Text";
+import { useForm } from "@tanstack/react-form";
+import { InlineSpinner } from "../../Common/Spinner";
+import { useDocument } from "src/context/document/hooks/useDocument";
+import map from "lodash/map";
+import { useMemo } from "react";
+import { BaseDocumentProps } from "../index.types";
+
+function DeleteDocumentFolder({ documentState }: BaseDocumentProps) {
+  const { deleteDocumentFolder } = useDocument();
+  const {
+    documentFolders: { selectedDocumentFolders },
+  } = documentState;
+  const isMultipleFoldersSelected = selectedDocumentFolders.length > 1;
+
+  const selectedFolderIds = useMemo(
+    () => map(selectedDocumentFolders, "id"),
+    [selectedDocumentFolders]
+  );
+
+  const Form = useForm({
+    onSubmit: async ({ value }) => {
+      deleteDocumentFolder.mutate(selectedFolderIds);
+    },
+  });
+
+  return (
+    <OverlayMenu
+      width={300}
+      minHeight={150}
+      side="outside-top"
+      anchorOffset={10}
+      alignmentOffset={0}
+      align="center"
+      anchorComponent={
+        isMultipleFoldersSelected ? (
+          <Button
+            variant="danger"
+            leadingVisual={PiTrashBold}
+            trailingVisual={() => (
+              <Text.Heading6>{selectedDocumentFolders.length}</Text.Heading6>
+            )}
+            disabled={false}
+            aria-label="Delete Selected Folders"
+            sx={{
+              flexShrink: 0,
+            }}
+          >
+            Delete Folders
+          </Button>
+        ) : (
+          <IconButton
+            variant="danger"
+            icon={PiTrashBold}
+            disabled={false}
+            aria-label="Delete Selected Folder"
+            sx={{
+              flexShrink: 0,
+            }}
+          />
+        )
+      }
+      heading={<Text.Heading5>Delete Selected Folder(s)</Text.Heading5>}
+      onCloseCallback={() => {}}
+    >
+      <Box
+        sx={{
+          p: 3,
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+        as="form"
+      >
+        <Text.ParagraphSecondary
+          sx={{
+            textAlign: "center",
+            marginBottom: 1,
+          }}
+        >
+          {`Are you sure you want to delete ${isMultipleFoldersSelected ? "these folders and all their contents" : "this folder and all its contents"}? This action cannot be undone.`}
+        </Text.ParagraphSecondary>
+        <Form.Subscribe
+          selector={({ isSubmitting }) => [isSubmitting]}
+          children={([isSubmitting]) => (
+            <Button
+              variant="danger"
+              leadingVisual={isSubmitting ? InlineSpinner : PiTrashBold}
+              disabled={isSubmitting}
+              onClick={Form.handleSubmit}
+              size="medium"
+            >
+              {isSubmitting
+                ? `Deleting ${isMultipleFoldersSelected ? "Folders" : "Folder"}...`
+                : `Delete ${isMultipleFoldersSelected ? "Folders" : "Folder"}`}
+            </Button>
+          )}
+        />
+      </Box>
+    </OverlayMenu>
+  );
+}
+
+export default DeleteDocumentFolder;
