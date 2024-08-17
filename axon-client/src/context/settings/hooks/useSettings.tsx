b4d68e999432e5d8c4635f3467dea6876e8e61d6
Stephen Sanwo <Stephen.sanwo@icloud.com>
2024-08-18 00:58:54 +0100

    checkpoint

--- axon-client/src/context/settings/hooks/useSettings.tsx
+++ axon-client/src/context/settings/hooks/useSettings.tsx
@@ -1,1 +1,29 @@
-export function useSettings() {}
+import { UseMutationResult } from "@tanstack/react-query";
+import { CreateColorDto } from "src/domain/settings/settings.dto";
+import {
+  ColorData,
+  ColorEntity,
+  SettingsQueryKeys,
+} from "src/domain/settings/settings.entity";
+import settingsService from "src/domain/settings/settings.service";
+import { useDataMutation } from "src/hooks/api/useDataMutation";
+
+export function useSettings(): {
+  createColor: UseMutationResult<ColorEntity, unknown, ColorData, unknown>;
+  deleteColor: UseMutationResult<boolean, unknown, string[], unknown>;
+} {
+  const createColor = useDataMutation<CreateColorDto, ColorEntity>({
+    mutationFn: async (dto: CreateColorDto) => settingsService.createColor(dto),
+    optionalQueryKeysToInvalidate: [[...SettingsQueryKeys.SETTINGS]],
+  });
+
+  const deleteColor = useDataMutation<string[], boolean>({
+    mutationFn: async (dto: string[]) => settingsService.deleteColor(dto),
+    optionalQueryKeysToInvalidate: [[...SettingsQueryKeys.SETTINGS]],
+  });
+
+  return {
+    createColor,
+    deleteColor,
+  };
+}
